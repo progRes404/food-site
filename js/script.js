3fc8fe82588e9e4acb6282e88ef020adb93b85df
Ilya Pokhilko <pokhilko.ilya@seznam.cz>
2022-10-25 22:33:15 +0200

    polyfills promise and foreach added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-    import tabs from './modules/tabs';
-    import modal from './modules/modal';
-    import timer from './modules/timer';
-    import cards from './modules/cards';
-    import calc from './modules/calc';
-    import forms from './modules/forms';
-    import slider from './modules/slider';
-    import {openModal} from './modules/modal';
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import {openModal} from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () => {
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
